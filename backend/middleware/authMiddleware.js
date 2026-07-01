a8da910687367645e0f82f7d7e0a914bee09dd4c
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-07-01 16:04:03 +0530

    Add JWT authentication middleware

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,40 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const protect = async (req, res, next) => {
+
+    try {
+
+        let token;
+
+        if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+            token = req.headers.authorization.split(" ")[1];
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+
+            if (!user) {
+                return res.status(401).json({
+                    message: "User not found",
+                });
+            }
+
+            req.user = user;
+
+            next();
+
+        } else {
+            return res.status(401).json({
+                message: "Not authorized, no token"
+            });
+        }
+
+    } catch (error) {
+        return res.status(401).json({
+            message: "Not authorized, token failed"
+        });
+    }
+
+}
+
+module.exports = { protect }
