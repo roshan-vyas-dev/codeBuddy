ecda5ad02a1f98c023262bee5942f4d4cadab935
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-07-10 12:48:59 +0530

    Implement delete comment API with authorization

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -41,5 +41,33 @@ const getComments = async (req, res) => {
     }
 }
 
-module.exports = { createComment, getComments };
+
+const deleteComment = async (req, res) => {
+    try {
+
+        const { id } = req.params;
+        const comment = await Comment.findById(id);
+
+        if (!comment) {
+            return res.status(404).json({ message: "Comment not found" })
+        }
+
+        if (comment.author.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Not authorized" })
+
+        }
+
+        await comment.deleteOne();
+
+        res.status(200).json({
+            message: "Comment deleted successfully"
+        });
+
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+module.exports = { createComment, getComments, deleteComment };
 
--- backend/routes/commentRoutes.js
+++ backend/routes/commentRoutes.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const {createComment,getComments} = require("../controllers/commentController")
+const {createComment,getComments,deleteComment} = require("../controllers/commentController")
 const { protect } = require("../middleware/authMiddleware");
 
 
 router.post("/",protect,createComment);
 router.get("/snippet/:id",protect,getComments);
+router.delete("/:id",protect,deleteComment);
 
 module.exports = router;
