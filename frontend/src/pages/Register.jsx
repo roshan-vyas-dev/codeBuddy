8884badfa7cbf4e85f63f0af8328a6fd0f07a6d8
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-07-15 15:06:21 +0530

    Implement user registration page

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useState } from "react"
+import axios from "axios"
+import { Link, useNavigate } from "react-router-dom"
+
 
 function Register() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+
+      const response = await axios.post(
+        "http://localhost:5000/api/auth/register",
+        {
+          username,
+          email,
+          password
+        }
+      );
+
+
+
+      navigate("/login");
+
+
+    } catch (error) {
+      console.log(error);
+
+    }
+  }
+
+
   return (
     <div>
-      
+      <form onSubmit={handleSubmit}>
+
+        <input type="text" value={username} placeholder="Enter username" onChange={(e) => setUsername(e.target.value)} />
+        <input type="email" value={email} placeholder="Enter email" onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" value={password} placeholder="Enter password" onChange={(e) => setPassword(e.target.value)} />
+
+
+        <button type="submit">Regsiter</button>
+
+
+      </form>
+
+      <p>
+        Already have an account?
+
+        <Link to="/login">Login</Link>
+      </p>
+
     </div>
   )
 }
