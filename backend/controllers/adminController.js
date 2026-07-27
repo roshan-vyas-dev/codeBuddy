f72595318450e094e074c7f1fc2a3bf9122ee2f1
Roshan Vyas <roshan.vyas.dev@gmail.com>
2026-07-27 20:29:28 +0530

    Add admin delete user functionality

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -99,4 +99,37 @@ const unblockUser = async (req, res) => {
 };
 
 
-module.exports = { getAllUsers, blockUser, unblockUser }
+const deleteUser = async (req, res) => {
+
+    try {
+
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+
+        if (user.role === "admin") {
+            return res.status(400).json({ message: "Admin cannot be deleted" })
+        }
+
+
+        await User.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            message: "User deleted successfully"
+        });
+
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+
+};
+
+
+module.exports = { getAllUsers, blockUser, unblockUser, deleteUser }
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,12 +3,13 @@ const router = express.Router();
 
 const { protect, adminOnly } = require("../middleware/authMiddleware");
 
-const { getAllUsers,blockUser,unblockUser } = require("../controllers/adminController");
+const { getAllUsers,blockUser,unblockUser,deleteUser } = require("../controllers/adminController");
 
 
 router.get("/users", protect, adminOnly, getAllUsers);
 router.patch("/users/:id/block",protect,adminOnly,blockUser)
 router.patch("/users/:id/unblock",protect,adminOnly,unblockUser)
+router.delete("/users/:id",protect,adminOnly,deleteUser)
 
 
 module.exports=router;
